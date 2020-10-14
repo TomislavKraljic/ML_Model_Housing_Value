fb6a4c3a654f38c5f08fddfeef57c7fe1788ed55
TomislavKraljic <tkraljic0@gmail.com>
2020-10-14 15:27:21 -0700

    push to github

---
+++ MedianHouseValue/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedianHouseValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<HousingData>("./housing.csv", hasHeader: true, separatorChar: ',');
+
+            var split = context.Data.TrainTestSplit(data, testFraction: 0.2); //This will split our data into two: Train and Test with 20%.
+
+            var features = split.TrainSet.Schema
+                .Select(col => col.Name)
+                .Where(colName => colName != "Label" && colName != "OceanProximity")
+                .ToArray();
+
+            var pipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+                .Append(context.Transforms.Concatenate("Features", features))
+                .Append(context.Transforms.Concatenate("Feature", "Features", "Text"))
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression());
+
+            var model = pipeline.Fit(split.TrainSet);
+
+            var predictions = model.Transform(split.TestSet);
+
+            var metrics = context.Regression.Evaluate(predictions);
+
+            Console.WriteLine($"R^2 - {metrics.RSquared}");
+        }
+
+
+    }
+}
